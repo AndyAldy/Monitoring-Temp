b8ef1beaa032d8c4eb9e89e594c30bc7c4973d65
AndyAldy <andyaldyansyah11@gmail.com>
2026-05-12 16:06:47 +0700

    Update project and remove Firebase credentials

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
   // TODO: Add the dependencies for Firebase products you want to use
   // When using the BoM, don't specify versions in Firebase dependencies
   implementation("com.google.firebase:firebase-analytics")
-  implementation 'androidx.multidex:multidex:2.0.1'
+  implementation("androidx.multidex:multidex:2.0.1")
 
 
   // Add the dependencies for any other desired Firebase products
@@ -43,7 +43,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled true
+        multiDexEnabled = true
     }
 
     buildTypes {
